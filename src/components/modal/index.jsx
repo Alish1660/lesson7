b7e09e608d98767d0e44f256c5355548a35fe17a
Alish1660 <git>
2024-07-02 15:19:41 +0500

    second

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,6 @@
+import LogOutModal from "./logout";
+import VerifyModal from "./verify";
+import SignInModal from "./signin";
+import Service from "./service";
+
+export { LogOutModal, VerifyModal, SignInModal, Service };
