7829c033e5bd926f5048da7fe4e57b480e166b80
Alish1660 <git>
2024-07-04 12:20:49 +0500

    order

---
+++ src/components/modal/order/index.jsx
@@ -0,0 +1,169 @@
+import * as React from "react";
+import {
+  Modal,
+  Backdrop,
+  Box,
+  Typography,
+  Button,
+  TextField,
+} from "@mui/material";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { serviceValidationSchema } from "../../../utils/validation";
+import order from "../../../service/order";
+
+const Fade = ({ children, in: open }) => {
+  const style = {
+    opacity: open ? 1 : 0,
+    transition: "opacity 0.5s",
+  };
+
+  return <div style={style}>{open ? children : null}</div>;
+};
+
+const Index = ({ open, handleClose }) => {
+  //   console.log(item, "item");
+  //   const initialValues = {
+  //     name: item?.name ? item?.name : "",
+  //     price: item?.price ? item?.price : "",
+  //   };
+
+  //   const handleSubmit = async (values) => {
+  //     const payload = { id: item.id, ...values };
+  //     if (item) {
+  //       try {
+  //         const response = await service.update(payload);
+  //         if (response.status === 200) {
+  //           window.location.reload();
+  //         }
+  //         handleClose();
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     } else {
+  //       try {
+  //         const response = await service.create(values);
+  //         if (response.status === 201) {
+  //           window.location.reload();
+  //         }
+  //         handleClose();
+  //       } catch (error) {
+  //         console.error("Error creating service:", error);
+  //       }
+  //     }
+  //   };
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h5" sx={{ my: 2, textAlign: "center" }}>
+            Create Service
+          </Typography>
+          <Formik
+          // initialValues={initialValues}
+          // onSubmit={handleSubmit}
+          // validationSchema={serviceValidationSchema}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Field
+                  name="name"
+                  type="text"
+                  as={TextField}
+                  label="Name"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  helperText={<ErrorMessage name="name" />}
+                />
+                <Field
+                  name="price"
+                  type="number"
+                  as={TextField}
+                  label="Price"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  helperText={<ErrorMessage name="price" />}
+                />
+
+                <Field
+                  name="name"
+                  type="text"
+                  as={TextField}
+                  label="Name"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  helperText={<ErrorMessage name="name" />}
+                />
+                <Field
+                  name="price"
+                  type="number"
+                  as={TextField}
+                  label="Price"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  helperText={<ErrorMessage name="price" />}
+                />
+                <Field
+                  name="name"
+                  type="text"
+                  as={TextField}
+                  label="Name"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  helperText={<ErrorMessage name="name" />}
+                />
+                <Field
+                  name="price"
+                  type="number"
+                  as={TextField}
+                  label="Price"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  helperText={<ErrorMessage name="price" />}
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  disabled={isSubmitting}
+                  sx={{ marginBottom: "8px" }}
+                >
+                  {isSubmitting ? "Saving..." : "Order"}
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Fade>
+    </Modal>
+  );
+};
+
+export default Index;
---
+++ src/service/order.js
@@ -0,0 +1,6 @@
+import http from "./config";
+
+const order = {
+  create: (data) => http.post("/order", data),
+};
+export default order;
